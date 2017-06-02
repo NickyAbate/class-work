4b5f2f1e01291b83969cc1cae7bda91d68864b42
NickyAbate <lofieloomis@gmail.com>
2017-06-02 11:10:06 -0700

    added stuff or whatever

--- awesome.js
+++ awesome.js
@@ -20,31 +20,38 @@ const app = new Vue({
 		
 		new Post(
 				'I just feel like I know you Rick...',
-				'https://google.com',
+				'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
 				'Kyle Poch',
 				'https://www.thememo.com/wp-content/uploads/2016/02/happy-birthday-rick-astley.jpg',
 				'All my life I\'ve just felt so close to you, Rick, like I understand you on a deeper level. We should meet up'
 				),
 		new Post(
 				'I\'ve always felt the same way',
-				'https://google.com',
-				'Byle Koch',
+				'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+				'Carrot Top',
 				'https://www.thememo.com/wp-content/uploads/2016/02/happy-birthday-rick-astley.jpg',
 				'I don\'t understand, I\'ve always felt the same way. This is weird, we are all so similar'
 				),
 		new Post(
-				'I\'ve been searching for a long time',
-				'https://google.com',
+				'I\'ve found it',
+				'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
 				'Marley\'s brother',
 				'https://www.thememo.com/wp-content/uploads/2016/02/happy-birthday-rick-astley.jpg',
-				'I think I\'ve found the answer. You have to believe me, they are cloning us to take over the world'
+				'They are cloning us. We are created by the UK to take over the world.'
 				),
 		new Post(
-				'I\'ve been searching for a long time',
-				'https://google.com',
-				'Marley\'s brother',
+				'This needs to stop',
+				'https://www.youtube.com/watch?v=2gfRtgMWUgs',
+				'Angry Ginger Kid',
 				'https://www.thememo.com/wp-content/uploads/2016/02/happy-birthday-rick-astley.jpg',
-				'I think I\'ve found the answer. You have to believe me, they are cloning us to take over the world'
+				'People like this (https://www.youtube.com/watch?v=2gfRtgMWUgs) are horrible for us. If we are clones we need to come together and stand up for ourselves!'
+				),
+		new Post(
+				'Come Together!',
+				'https://www.youtube.com/watch?v=GM12Sda56jE',
+				'Ginger Jesus Man',
+				'https://pics.onsizzle.com/jesus-was-a-ginger-thats-why-he-was-crucified-7405963.png',
+				'We need to take action! (https://www.youtube.com/watch?v=2gfRtgMWUgs)'
 				),
 
 			
